930c75c0b11d66d60aa11bb9ea6936ca72190d17
tadpoleAngel <opc4775@gmail.com>
2026-01-24 10:58:18 -0500

    pose creator uses AprilTagFiledLayout now

--- src/main/java/frc/robot/subsystems/util/PoseCreator.java
+++ src/main/java/frc/robot/subsystems/util/PoseCreator.java
@@ -1,46 +1,25 @@
 package frc.robot.subsystems.util;
 
-import java.io.FileReader;
-import java.util.HashMap;
-import java.util.Map;
-
-
+import java.io.IOException;
 import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 
 public class PoseCreator {
 
-    private TagPos getTagPos(double tagId) {
-        
+    private Pose3d getTagPos(int tagId) throws IOException{
+        AprilTagFieldLayout tagLayout = new AprilTagFieldLayout("./AprilTags.json");
+
+        return tagLayout.getTagPose(tagId).get();
     }
 
 
-    public Pose3d estimateRobot (int tagId, Transform3d aprilTagtoCamera, Transform3d cameraToRobot) {
-        return tagPoses.get(tagId).plus(aprilTagtoCamera.plus(cameraToRobot).inverse()); //TODO: not sure about this .inverse()
+    public Pose3d estimateRobot (int tagId, Transform3d aprilTagtoCamera, Transform3d cameraToRobot) throws IOException{
+        return getTagPos(tagId).plus(aprilTagtoCamera.plus(cameraToRobot).inverse()); //TODO: not sure about this .inverse()
     }
 
-    public Pose3d estimateTurret (int tagId, Transform3d aprilTagtoCamera, Transform3d cameraToRobot, Transform3d robotToTurret) {
+    public Pose3d estimateTurret (int tagId, Transform3d aprilTagtoCamera, Transform3d cameraToRobot, Transform3d robotToTurret) throws IOException{
         return this.estimateRobot(tagId, aprilTagtoCamera, cameraToRobot).plus(robotToTurret);
-    }
-
-    private class TagPos {
-        public Pose3d pose;
-        public Rotation3d rotation;
-
-        public TagPos (Pose3d pose, Rotation3d rotation) {
-            this.pose = pose;
-            this.rotation = rotation;
-        }
-
-        public Pose3d getPose() {
-            return this.pose;
-        }
-
-        public Rotation3d getRotation() {
-            return this.rotation;
-        }
     }
 }
