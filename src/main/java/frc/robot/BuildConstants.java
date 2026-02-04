d6ab0ae8879d66bab5cff2cb5a970804cbc92492
tadpoleAngel <opc4775@gmail.com>
2026-02-03 19:54:43 -0500

    commented out all the broken stuff but seemed like it kinda worked

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 39;
-  public static final String GIT_SHA = "3174291a344770ca84b662ea4f89f8ce729809f3";
-  public static final String GIT_DATE = "2026-02-01 20:22:00 EST";
+  public static final int GIT_REVISION = 40;
+  public static final String GIT_SHA = "9d302af2ed6b21a5eee751069a0ab42f4b22e48a";
+  public static final String GIT_DATE = "2026-02-02 19:10:42 EST";
   public static final String GIT_BRANCH = "oc_turret_pos";
-  public static final String BUILD_DATE = "2026-02-02 19:10:26 EST";
-  public static final long BUILD_UNIX_TIME = 1770077426682L;
+  public static final String BUILD_DATE = "2026-02-03 19:53:36 EST";
+  public static final long BUILD_UNIX_TIME = 1770166416211L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -8,31 +8,36 @@ public class IntakeSubsystem extends EntechSubsystem<IntakeInput, IntakeOutput> 
     @Override
     public void initialize() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+        // throw new UnsupportedOperationException("Unimplemented method 'initialize'");
     }
 
     @Override
     public boolean isEnabled() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isEnabled'");
+        // throw new UnsupportedOperationException("Unimplemented method 'isEnabled'");
+        return false;
     }
 
     @Override
     public void updateInputs(IntakeInput input) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateInputs'");
+        // throw new UnsupportedOperationException("Unimplemented method 'updateInputs'");
     }
 
     @Override
     public Command getTestCommand() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTestCommand'");
+        // throw new UnsupportedOperationException("Unimplemented method 'getTestCommand'");
+        return new Command() {
+            
+        };
     }
 
     @Override
     protected IntakeOutput toOutputs() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toOutputs'");
+        // throw new UnsupportedOperationException("Unimplemented method 'toOutputs'");
+        return new IntakeOutput();
     }
     
 }
