79f2692c71e54efa207b3d537a421c4512c17578
tadpoleAngel <opc4775@gmail.com>
2026-03-12 16:34:24 -0400

    make robot not crash

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 268;
-  public static final String GIT_SHA = "01002a6a3ce229de298b92561837f0a0b94f2ba3";
-  public static final String GIT_DATE = "2026-03-12 14:53:07 EDT";
+  public static final int GIT_REVISION = 269;
+  public static final String GIT_SHA = "cc03af4e30b533293aeb6d725c6f13e95ee3e3a8";
+  public static final String GIT_DATE = "2026-03-12 16:14:56 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-12 16:07:05 EDT";
-  public static final long BUILD_UNIX_TIME = 1773346025891L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-03-12 16:32:46 EDT";
+  public static final long BUILD_UNIX_TIME = 1773347566835L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
