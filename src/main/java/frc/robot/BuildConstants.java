de4abc89e759e021c62db002e1237ac5bff90867
WhyDoesGodDoThis <101780103+WhyDoesGodDoThis@users.noreply.github.com>
2026-03-14 13:41:06 -0400

    pre tour 2

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 293;
-  public static final String GIT_SHA = "c1a4e4bdbba3831f9b5d67d2ced3eaa5b0af50a2";
-  public static final String GIT_DATE = "2026-03-14 10:55:47 EDT";
+  public static final int GIT_REVISION = 295;
+  public static final String GIT_SHA = "8d9fd959828ad213b32a8385673804ccf2c91e28";
+  public static final String GIT_DATE = "2026-03-14 12:26:35 EDT";
   public static final String GIT_BRANCH = "Richland_main";
-  public static final String BUILD_DATE = "2026-03-14 11:40:37 EDT";
-  public static final long BUILD_UNIX_TIME = 1773502837034L;
+  public static final String BUILD_DATE = "2026-03-14 13:15:17 EDT";
+  public static final long BUILD_UNIX_TIME = 1773508517800L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/util/TurretCalculator.java
+++ src/main/java/frc/robot/util/TurretCalculator.java
@@ -23,9 +23,18 @@ public class TurretCalculator {
     }
 
     public double calculateTargetTurretAngle() {
+        // RobotConstants.
+        // target = new Pose2d(target.getX(), -target.getY(), target.getRotation());
         Translation2d targetTranslation = target.getTranslation().plus(
                 robotPose.getTranslation().plus(RobotConstants.TURRET.TURRET_OFFSET.rotateBy(robotPose.getRotation())));
-        return targetTranslation.getAngle().getDegrees() + robotPose.getRotation().getDegrees();
+
+        Translation2d turretToRobot = RobotConstants.TURRET.TURRET_OFFSET.rotateBy(robotPose.getRotation());
+        Pose2d turretPose = new Pose2d(robotPose.getX() - turretToRobot.getX(), robotPose.getY() - turretToRobot.getY(),
+                robotPose.getRotation());
+
+        double angle = Math.toDegrees(Math.atan2(target.getY() - turretPose.getY(), target.getX() - turretPose.getX()));
+
+        return ((-robotPose.getRotation().getDegrees()) + (-angle) % 180) + 180;
     }
 
     public boolean isValidTurretAngle(double angle, double toleranceDegrees) {
