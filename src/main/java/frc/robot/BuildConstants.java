c9b453ce374fef497b207af830f3c6488b2f8639
WhyDoesGodDoThis <101780103+WhyDoesGodDoThis@users.noreply.github.com>
2026-03-27 13:35:22 -0400

    right auto no go through trench sideways

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 345;
-  public static final String GIT_SHA = "163717c4063e184a00cfc86cff21f120bb22b1f6";
-  public static final String GIT_DATE = "2026-03-27 11:42:01 EDT";
+  public static final int GIT_REVISION = 346;
+  public static final String GIT_SHA = "47d313882144536cc0af77a57fc418f4d2604618";
+  public static final String GIT_DATE = "2026-03-27 11:43:35 EDT";
   public static final String GIT_BRANCH = "hartsville_main";
-  public static final String BUILD_DATE = "2026-03-27 11:42:08 EDT";
-  public static final long BUILD_UNIX_TIME = 1774626128837L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-03-27 13:35:00 EDT";
+  public static final long BUILD_UNIX_TIME = 1774632900813L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
