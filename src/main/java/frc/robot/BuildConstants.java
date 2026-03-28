6ce27d3419e094677b2e0401cc1d527a2b049b8b
WhyDoesGodDoThis <101780103+WhyDoesGodDoThis@users.noreply.github.com>
2026-03-28 08:49:36 -0400

    idk what they did

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 351;
-  public static final String GIT_SHA = "8bcf3012e9e5907f99962846a76e4ff1e572bba7";
-  public static final String GIT_DATE = "2026-03-27 18:29:43 EDT";
+  public static final int GIT_REVISION = 353;
+  public static final String GIT_SHA = "5859e9bbd7df6794b2203a1aff7bc1903b97bd61";
+  public static final String GIT_DATE = "2026-03-28 08:08:14 EDT";
   public static final String GIT_BRANCH = "hartsville_main";
-  public static final String BUILD_DATE = "2026-03-27 19:31:27 EDT";
-  public static final long BUILD_UNIX_TIME = 1774654287580L;
+  public static final String BUILD_DATE = "2026-03-28 08:40:42 EDT";
+  public static final long BUILD_UNIX_TIME = 1774701642204L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
