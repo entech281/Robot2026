2e1b97321b7a9492d84e3403646be0fbde3fe4ff
tadpoleAngel <opc4775@gmail.com>
2026-04-04 13:05:28 -0400

    variable snowblow two

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 360;
-  public static final String GIT_SHA = "79115d9f3ecfbaf864e632c07d654db172f87788";
-  public static final String GIT_DATE = "2026-03-31 17:44:56 EDT";
-  public static final String GIT_BRANCH = "oc_variable_snowblow_target";
-  public static final String BUILD_DATE = "2026-04-04 13:00:16 EDT";
-  public static final long BUILD_UNIX_TIME = 1775322016631L;
+  public static final int GIT_REVISION = 363;
+  public static final String GIT_SHA = "d2bf28cb39bbbe6e9d0dad4edf34620e1ee22369";
+  public static final String GIT_DATE = "2026-04-04 13:04:34 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-04-04 13:05:20 EDT";
+  public static final long BUILD_UNIX_TIME = 1775322320488L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
