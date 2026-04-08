741d527783a71708df85dba37f8d0e081fe52be5
Paul Manavalan <paulmanavalan1@gmail.com>
2026-04-08 19:17:16 -0400

    added auto

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 373;
-  public static final String GIT_SHA = "0f62ef2f333211f637511a07cca32a82a03faba3";
-  public static final String GIT_DATE = "2026-04-06 19:49:42 EDT";
-  public static final String GIT_BRANCH = "oc_shooter_max";
-  public static final String BUILD_DATE = "2026-04-07 19:46:26 EDT";
-  public static final long BUILD_UNIX_TIME = 1775605586212L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 377;
+  public static final String GIT_SHA = "c53da6199be8803522b5403f509d9322c99e7791";
+  public static final String GIT_DATE = "2026-04-07 21:27:51 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-04-08 18:57:26 EDT";
+  public static final long BUILD_UNIX_TIME = 1775689046219L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
