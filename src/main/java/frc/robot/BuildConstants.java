4122fa8772dd043e6c7569886380cf4d06cfa4f3
tadpoleAngel <opc4775@gmail.com>
2026-04-10 12:23:37 -0400

    more shoot time

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 396;
-  public static final String GIT_SHA = "e6c740c19f1799e02ba467895fe9872a1a8965af";
-  public static final String GIT_DATE = "2026-04-10 11:36:53 EDT";
+  public static final int GIT_REVISION = 397;
+  public static final String GIT_SHA = "7d8c51f4fc944d449bcfb83d9c1efa45bf180b23";
+  public static final String GIT_DATE = "2026-04-10 11:42:32 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-04-10 11:40:10 EDT";
-  public static final long BUILD_UNIX_TIME = 1775835610706L;
+  public static final String BUILD_DATE = "2026-04-10 12:23:09 EDT";
+  public static final long BUILD_UNIX_TIME = 1775838189525L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
